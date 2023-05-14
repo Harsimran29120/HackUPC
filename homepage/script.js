7c600eeb233d6ff51f2ad3a9ecc1466749d1e5f9
Harsimran29120 <72358179+Harsimran29120@users.noreply.github.com>
2023-05-14 02:56:58 +0200

    Add files via upload

---
+++ homepage/script.js
@@ -0,0 +1,50 @@
+// const options = ['chat', 'games', 'music', 'movies'];
+
+// const memberDiv = document.querySelector('.memberDiv');
+// const seeMoreIcon = document.querySelector('.seeMoreIcon');
+// // const btn1 = document.querySelector(".btn:nth-child(1)");
+// const btn2 = document.querySelector(".btn:nth-child(2)");
+// const btn3 = document.querySelector(".btn:nth-child(3)");
+// const btn4 = document.querySelector(".btn:nth-child(4)");
+
+// const userIcons = () => {
+//     options.reverse();
+//     options.map((curElem) => {
+//         memberDiv.insertAdjacentHTML('afterbegin', `
+//         <button class="btn"><span>${curElem}</span></button>
+//         `);
+//     })
+// };
+
+// btn1.addEventListener("click", () => {
+//     window.open("C:/Users/lydia/vahoot.py");
+// });
+
+// btn2.addEventListener("click", () => {
+//     window.open("C:/Users/lydia/vahoot.py");
+// });
+
+// btn3.addEventListener("click", () => {
+//     window.open("C:/Users/lydia/vahoot.py");
+// }); 
+
+// btn4.addEventListener("click", () => {
+//     window.open("C:/Users/lydia/vahoot.py");
+// }); 
+
+// seeMoreIcon.addEventListener('click', () => {
+//     alert("more is coming soon, but for now, stick to the other options! and fly vueling next time you're jet set to see the updates :)")
+    // let userName = prompt('please enter your name');
+
+    // if(userName != null && !options.includes(userName)){
+    //     options.push(userName);
+    //     console.log(options);
+    //     memberDiv.insertAdjacentHTML('afterbegin', `
+    //     <button class="btn"><span>${userName}</span></button>
+    //     `);
+    // }else{
+    //     alert('username already exist');
+//     // }
+//     })
+
+// userIcons();
